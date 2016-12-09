a07ff02063a91cf9bc8abc314fe58655ef9fa284
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2016-12-09 10:01:10 +0100

    Added route to contents

--- dummyhub.py
+++ dummyhub.py
@@ -3,7 +3,7 @@ app = flask.Flask(__name__)
 
 @app.route("/")
 def dummyhub():
-    return "DummyHub here! Try other routes, like /albertmeronyo/lodapi or /contents"
+    return "DummyHub here! Try other routes, like /repos/albertmeronyo/lodapi or /repos/albertmeronyo/lodapi/contents"
 
 @app.route("/repos/albertmeronyo/lodapi")
 def dummyapi():
@@ -14,6 +14,13 @@ def dummyapi():
     d['owner']['html_url'] = "http://dummy/url"
 
     return flask.jsonify(**d)
+
+@app.route("/repos/albertmeronyo/lodapi/contents")
+def contents():
+    d = []
+    d.append({'name' : "q1.rq"})
+
+    return flask.jsonify(**d) 
 
 if __name__ == "__main__":
     app.run(port=8089, debug=True)
