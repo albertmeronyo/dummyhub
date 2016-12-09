0c04fd442dd1bbe5821357fb5fa7f3dd13a6258a
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2016-12-09 10:14:11 +0100

    Fix routes

--- dummyhub.py
+++ dummyhub.py
@@ -5,9 +5,9 @@ app = flask.Flask(__name__)
 
 @app.route("/")
 def dummyhub():
-    return "DummyHub here! Try other routes, like /repos/albertmeronyo/lodapi or /repos/albertmeronyo/lodapi/contents"
+    return "DummyHub here! Try other routes, like /repos/albertmeronyo/sp2b-queries or /repos/albertmeronyo/sp2b-queries/contents"
 
-@app.route("/repos/albertmeronyo/lodapi")
+@app.route("/repos/albertmeronyo/sp2b-queries")
 def dummyapi():
     d = {}
     d['name'] = "DUMMY API"
@@ -17,12 +17,12 @@ def dummyapi():
 
     return flask.jsonify(**d)
 
-@app.route("/repos/albertmeronyo/lodapi/contents")
+@app.route("/repos/albertmeronyo/sp2b-queries/contents")
 def contents():
     d = []
     d.append({'name' : "q1.rq"})
 
-    return json.dumps(d) 
+    return json.dumps(d)
 
 if __name__ == "__main__":
     app.run(port=8089, debug=True)
