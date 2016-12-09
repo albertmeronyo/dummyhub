fe96afb1e42dfa1430ce008853acf1926f7d6ac1
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2016-12-09 09:35:39 +0100

    First commit

---
+++ dummyhub.py
@@ -0,0 +1,19 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def dummyhub():
+    return "DummyHub here! Try other routes, like /albertmeronyo/lodapi or /contents"
+
+@app.route("/albertmeronyo/lodapi")
+def dummyapi():
+    d = {}
+    d['name'] = "DUMMY API"
+    d['owner'] = {}
+    d['owner']['login'] = "albertmeronyo (DUMMY)"
+    d['owner']['html_url'] = "http://dummy/url"
+
+    return flask.jsonify(**d) 
+
+if __name__ == "__main__":
+    app.run(port=8089, debug=True)
